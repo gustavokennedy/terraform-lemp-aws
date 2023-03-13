d236ae4e50856f0d92c322e5e9881fd39e02d847
Gustavo Kennedy Renkel <gustavo@overall.cloud>
2023-03-13 18:55:14 +0000

    Arquivo configuração inicial

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "1.4.0"
+
+  required_providers {
+
+  aws = {
+	source = "hashicorp/aws"
+	version = "4.58.0"
+}
+}
+}
+
+
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "tf01"
+}
+
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "my-tf-test-bucket-kennedyrenkel"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
