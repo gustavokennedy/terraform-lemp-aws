238060af38044079cd33fb6bb507699394fc914a
Gustavo Kennedy Renkel <98914036+gustavokennedy@users.noreply.github.com>
2023-03-13 15:57:23 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,25 +3,23 @@ terraform {
 
   required_providers {
 
-  aws = {
-	source = "hashicorp/aws"
-	version = "4.58.0"
+  	aws = {
+		source = "hashicorp/aws"
+		version = "4.58.0"
+		}
+	}
 }
-}
-}
-
-
 
 provider "aws" {
-  region = "us-east-1"
-  profile = "tf01"
+	  region = "us-east-1"
+	  profile = "tf01"
 }
 
 resource "aws_s3_bucket" "test-bucket" {
-  bucket = "my-tf-test-bucket-kennedyrenkel"
+	  bucket = "my-tf-test-bucket-kennedyrenkel"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+	tags = {
+		Name        = "My bucket"
+		Environment = "Dev"
+		}
+	}
