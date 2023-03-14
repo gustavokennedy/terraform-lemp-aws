9fa6f964659f83cedb752421e765263e1be0e412
Gustavo Kennedy Renkel <98914036+gustavokennedy@users.noreply.github.com>
2023-03-14 18:47:12 -0300

    Update lightsail.tf

--- lightsail.tf
+++ lightsail.tf
@@ -11,18 +11,12 @@ provider "aws" {
   region                   = var.region
 }
 
-# Cria chave de acesso para instância
-#resource "aws_lightsail_key_pair" "instance_key_pair" {
-#  name = var.instance
-#}
-
 # Cria a instância na Lightsail
 resource "aws_lightsail_instance" "instance" {
   name              = var.instance
   availability_zone = var.instance_availability_zone
   blueprint_id      = var.instance_blueprintid
   bundle_id         = var.instance_bundleid
-  key_pair_name     = var.instance
   tags = {
     Environment = "Production"
   }
