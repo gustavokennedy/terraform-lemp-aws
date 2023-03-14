792ba7c669973b98cbc2d9a24293ae43b1048ef6
Gustavo Kennedy Renkel <98914036+gustavokennedy@users.noreply.github.com>
2023-03-14 20:45:24 -0300

    Update lightsail.tf

--- lightsail.tf
+++ lightsail.tf
@@ -62,9 +62,10 @@ resource "aws_lightsail_instance_public_ports" "instance" {
     to_port   = 80
   }
   
-  provisioner "file" {
-    source      = "conf/setup.sh"
-    destination = "~/setup.sh"
-  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y supervisor"
+    ]
   
 }
